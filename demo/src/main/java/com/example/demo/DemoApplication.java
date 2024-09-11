01eb7462669bcbcb5e57b2b664cb586702596000
Гулминавар Багланкызы <gulminavarbaglankyzy@MacBook-Air-Gulminavar.local>
2024-09-11 10:14:58 +0500

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//	this is a test comment from Gulminavar
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
