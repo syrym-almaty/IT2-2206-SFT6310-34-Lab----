617608e096543c95474f0b9b19048c4f4cc7fb0a
qymyz <qazybek648@gmail.com>
2024-09-11 11:26:13 +0500

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 // this is my comments kazbek
-
+// test 2
 //	ssh -T git@github.com this is a command to connect through ssh connect
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
