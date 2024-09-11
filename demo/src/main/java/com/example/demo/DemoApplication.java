af6e1bc7479158ea722f4e4e82aac83bfd7ad9e4
qymyz <qazybek648@gmail.com>
2024-09-11 11:35:53 +0500

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 // this is my comments kazbek
 // test 2
 //	ssh -T git@github.com this is a command to connect through ssh connect
+//	test
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
