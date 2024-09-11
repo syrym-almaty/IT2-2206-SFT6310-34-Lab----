c7d5937035676880aaace446de51497176700e57
Qazybek Kõpzhasar <qazybekkopzhasar@Qazybeks-MacBook-Air.local>
2024-09-11 11:19:54 +0500

    test comments

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 // this is my comments kazbek
+
+//	ssh -T git@github.com this is a command to connect through ssh connect
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
