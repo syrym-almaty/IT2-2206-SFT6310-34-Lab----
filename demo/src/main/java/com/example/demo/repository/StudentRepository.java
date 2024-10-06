b20bd5d439eff9af6588e38acdbba88d822c0f9f
Zharkyn <zharkynmoldashov@mail.ru>
2024-10-07 00:40:04 +0500

    adding get request, repository, and service to find student by name, but its useless because name of students can be repeated. So next commit and push i will add some parametres to entity Student. :)

--- demo/src/main/java/com/example/demo/repository/StudentRepository.java
+++ demo/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
